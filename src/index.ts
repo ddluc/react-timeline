3d693e60e41b8beba7ad623356706b4ec981ab1d
Daniel Lucas <daniel@ddluc.com>
2021-12-31 09:35:13 -0800

    Add font to production build

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 // Import the main app package
 import Package from './lib'; 
 
-
-// If the package is in development mode, rende
-if (process.env.NODE_ENV === 'development') {
-  // Render the app
-  Package.render(); 
-}
+// Inject the package into the html file
+Package.render(); 
 
 export default Package.ReactTimeline; 
